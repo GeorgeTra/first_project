94ff9686faddbc5ba2e90eb0ae13df687789a514
georg <georgij_trakhten@mail.ru>
2023-03-12 20:56:48 +0300

    Task2 added

---
+++ venv/Lesson2/Lesson2.py
@@ -0,0 +1,73 @@
+# a = 5
+# print(type(a), id(a))
+#
+# a = "Hello world!"
+# print(type(a), id(a))
+#
+# a = 5 * 2 / 3
+# print(type(a), id(a))
+#
+# data = 42
+# print(isinstance(data, (int, str)))
+#
+# print(hash(str))
+
+# variable = input("Enter something: ")
+# print(type(variable), id(variable), hash(variable))
+
+# a: int = 42
+# b: float = float(input("Enter a number: "))
+# a = a / b
+# print(a)
+
+# help()
+
+# print(dir(str))
+# a = 'hello bib'
+# print(a.capitalize())
+
+# import sys
+#
+# print(sys.getsizeof(10))
+
+# data = [0, 1, 1, 2, 3, 5, 8, 13, 21]
+# while data:
+#     print(data.pop(), end=" ")
+# print(data)
+
+# empty_str = ''
+# en_str = 'Text'
+# ru_str = 'Текст'
+# unicode_str = '😀😍😉🙃'
+# print(empty_str.__sizeof__())
+# print(en_str.__sizeof__())
+# print(ru_str.__sizeof__())
+# print(unicode_str.__sizeof__())
+
+# text = input("Enter some text: ")
+# if text.isnumeric():
+#     text = int(text, base=36)
+#     print(bin(text))
+#     print(oct(text))
+#     print(hex(text))
+# else:
+#     print(text.isascii())
+
+# import math
+# import decimal
+# import fractions
+
+# print(math.sqrt(4))
+
+# f1 = fractions.Fraction(1, 3)
+# print(f1)
+# f2 = fractions.Fraction(3, 5)
+# print(f2)
+# print(f1 * f2)
+
+# print(pow(2, 2))
+
+# print(round(14,4545[, 3])
+
+print(pow(42, 5, 10))
+
